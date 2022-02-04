b2f490f06c9af90bc571e0a96eb7adc018cd23c0
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2022-02-04 20:11:41 +0100

    Add files via upload

--- src/SD_card_logger.cpp
+++ src/SD_card_logger.cpp
@@ -1,35 +1,33 @@
 #include "SD_card_logger.h"
 
-void sd_card_logger::init(String bootmessage)
+void sd_card_logger::init(String bootmessage, uint8_t CSpin)
 {
-    _bootmessage = bootmessage;
-   
+    if (CSpin != SD_DEFAULT_CS_PIN)
+        this->CSpin = CSpin;
 
-   
-    if (!SD.begin())
+    // init the SD card
+    if (!SD.begin(this->CSpin))
     {
 
-       //you can do something on fail if you want  add code here
+        // don't do anything more:
     }
-    sd_card_logger::_file = &SD.open(_filename, FILE_WRITE);
-    if (!this->_file)
+    _file = &SD.open(_filename, FILE_WRITE);
+    if (!_file)
     {
-         //you can do something on fail if you want  add code here
     }
     _file->println();
-    _file->println(_bootmessage);
+    _file->println(bootmessage);
     _file->close();
 }
+
 bool sd_card_logger::log(String logmessage, int logging_freq)
 {
-    this->logmessage = logmessage;
-    this->logging_freq = logging_freq;
     if (_timer->timer(logging_freq))
     {
 
-       
-        sd_card_logger::_file = &SD.open(_filename, FILE_WRITE);
-        if (!sd_card_logger::_file)
+        // check if it's been over 10 ms since the last line added
+        _file = &SD.open(_filename, FILE_WRITE);
+        if (!_file)
         {
             return false;
         }
--- src/SD_card_logger.h
+++ src/SD_card_logger.h
@@ -3,29 +3,28 @@
 #include <Arduino.h>
 #include <SD.h>
 #include <Simpletimer.h>
-
+#define SD_DEFAULT_CS_PIN 10
 class sd_card_logger
 {
 public:
-    String _filename;
-
-    int logging_freq;// in ms
-    String logmessage;
-    String _bootmessage;
-    sd_card_logger(String filename, File *file, Simpletimer *timer)
+    sd_card_logger(String filename, uint8_t CSpin = SD_DEFAULT_CS_PIN)
     {
+        this->CSpin = CSpin;
         _filename = filename;
-        _file = file;
-        _timer = timer;
+        _timer = new Simpletimer;
     }
-    void init(String bootmessage);// initialize with message // empty string if you dont want to init with message
-    bool log(String logmessage, int logging_freq);//log your message with given freq
+    ~sd_card_logger()
+    {
+        delete[] _timer;
+    }
+    void init(String bootmessage, uint8_t CSpin = SD_DEFAULT_CS_PIN);
+    bool log(String logmessage, int logging_freq);
 
 private:
-    Simpletimer *_timer;
-    File *_file;
-
-protected:
+    uint8_t CSpin;
+    Simpletimer *_timer = nullptr;
+    File *_file = nullptr;
+    String _filename;
     unsigned long lastMillis = 0;
 };
 
