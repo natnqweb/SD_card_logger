12f9f1c7a4d28ec215dc2140f72460c19ef42479
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2022-02-04 20:11:28 +0100

    Add files via upload

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,24 +1,28 @@
 #include <SD_card_logger.h>
+// ---------------------------- here you can check how to connect SD card reader to Arduino---------------------------------------
 
-File txtFile;    
-Simpletimer timer1;// in order to log text to sd you need my Simpletimerlibrary https://github.com/natnqweb/Simpletimer
-Simpletimer timer2;
+//       https://lastminuteengineers.com/arduino-micro-sd-card-module-tutorial/
+
+// ---------------------------- here you can check how to connect SD card reader to Arduino---------------------------------------
+  
+// in order to log to SD you need my Simpletimer library https://github.com/natnqweb/Simpletimer
+Simpletimer timer1;
 char buffer[30];
-
+uint8_t CS =10; // micro SD CS pin by default it is 10
 int i;
-sd_card_logger logger1("XY.txt", &txtFile, &timer1);// send timer and File object to sd_card_logger class;
+sd_card_logger logger1("XY.txt",CS);// create object
 void setup()
-{
+{// this is first line created in file
     logger1.init("X X^2");
 	//you can provide empty string if you want
 }
 void loop()
 {
-    if (timer2.timer(999))// entry every 999ms
+    if (timer1.timer(999))// entry every 999 ms
     {
         i++;
         sprintf(buffer, "%d %d\n", i, i * i);
     }
 
-    logger1.log(buffer, 1000);//logg buffer every 1 s
+    logger1.log(buffer, 1000);//logg buffer every 1 ms
 }
